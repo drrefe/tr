9f4f6b1c94f57f6b4ee312ce15009ccfdb7a23f1
Ramazan EFE <refe@ramazanefe.com>
2023-08-19 22:54:13 +0300

    v1.0.2

--- src/Tr.php
+++ src/Tr.php
@@ -7,33 +7,33 @@ class Tr
 	/**
 	 * Turkish safe upper case string (strtoupper)
 	 * 
-	 * @param string $value
+	 * @param string $text
 	 * @return string
 	 */
-	public static function upperCase(string $value): string
+	public static function upperCase(string $text): string
 	{
-		return mb_strtoupper(str_replace(['i', 'ı'], ['İ', 'I'], $value), "UTF-8");
+		return mb_strtoupper(str_replace(['i', 'ı'], ['İ', 'I'], $text), "UTF-8");
 	}
 
 	/**
 	 * Turkish safe lower case string (strtolower)
 	 * 
-	 * @param string $value
+	 * @param string $text
 	 * @return string
 	 */
-	public static function lowerCase(string $value): string
+	public static function lowerCase(string $text): string
 	{
-		return mb_strtolower(str_replace(['İ', 'I'], ['i', 'ı'], $value), "UTF-8");
+		return mb_strtolower(str_replace(['İ', 'I'], ['i', 'ı'], $text), "UTF-8");
 	}
 
 	/**
 	 * Turkish safe title string (ucfirst)
 	 * 
-	 * @param string $value
+	 * @param string $text
 	 * @return string
 	 */
-	public static function title(string $value): string
+	public static function title(string $text): string
 	{
-		return str_replace('i̇', 'i', ltrim(mb_convert_case(str_replace(array('i', 'I'), array('İ', 'ı'), $value), MB_CASE_TITLE, 'UTF-8')));
+		return str_replace('i̇', 'i', ltrim(mb_convert_case(str_replace(array('i', 'I'), array('İ', 'ı'), $text), MB_CASE_TITLE, 'UTF-8')));
 	}
 }
