a5eb3b99d696c5608905db9ddee438005acd4282
Ramazan EFE <refe@ramazanefe.com>
2023-08-21 01:09:21 +0300

    1.0.9

--- src/Core/Dictionary.php
+++ src/Core/Dictionary.php
@@ -3,18 +3,20 @@ namespace Drrefe\Tr\Core;
 
 class Dictionary
 {
-	public static $hardVowels = "aıou";
-	public static $softVowels = "eiöü";
-	public static $roundedVowels = "oöuü";
-	public static $hardConsonants = "pçtk";
+	public static $hardVowels = "aàáãâıoòóuùú";
+	public static $softVowels = "eêëèéæäâiôöüû";
+	public static $roundedVowels = "oôöòóuùúüû";
+	public static $hardConsonants = "pçtkfsş";
 	public static $softConsonants = "bcdğ";
 
 	public static $specialWordsEI = [
-		"kırklareli"
+		"kırklareli",
+		"eminönü"
 	];
 
 	public static $specialWordsDeDen = [
-		"kırklareli"
+		"kırklareli",
+		"eminönü"
 	];
 
 	public static $softenLastConsonantExceptions = [
