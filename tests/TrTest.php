43f9a5c573ad04ff68138b04c0b1ceae664e5a5e
Ramazan EFE <refe@ramazanefe.com>
2023-08-23 23:03:06 +0300

    v1.0.10

---
+++ tests/TrTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drrefe\Tr\Test;
+
+use Drrefe\Tr\Tr;
+use PHPUnit\Framework\TestCase;
+
+class TrTest extends TestCase
+{
+
+	public function testSuffix()
+	{
+		$rows = json_decode(file_get_contents(__DIR__ . "/test-rows.json"), true, JSON_UNESCAPED_UNICODE);
+		
+		foreach ($rows as $row) {
+			$this->assertSame($row["expected_e"],   Tr::suffix("e",   $row["text"], ["locale" => $row["locale"], "apostrophe" => $row["apostrophe"], "uppercase" => $row["uppercase"]]));
+			$this->assertSame($row["expected_i"],   Tr::suffix("i",   $row["text"], ["locale" => $row["locale"], "apostrophe" => $row["apostrophe"], "uppercase" => $row["uppercase"]]));
+			$this->assertSame($row["expected_in"],  Tr::suffix("in",  $row["text"], ["locale" => $row["locale"], "apostrophe" => $row["apostrophe"], "uppercase" => $row["uppercase"]]));
+			$this->assertSame($row["expected_de"],  Tr::suffix("de",  $row["text"], ["locale" => $row["locale"], "apostrophe" => $row["apostrophe"], "uppercase" => $row["uppercase"]]));
+			$this->assertSame($row["expected_den"], Tr::suffix("den", $row["text"], ["locale" => $row["locale"], "apostrophe" => $row["apostrophe"], "uppercase" => $row["uppercase"]]));
+		}
+	}
+}
