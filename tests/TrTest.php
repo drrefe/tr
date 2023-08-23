b099f73c8518207917bd4d7ca3823977bba949d8
Ramazan EFE <refe@ramazanefe.com>
2023-08-23 23:08:10 +0300

    v1.0.12

--- tests/TrTest.php
+++ tests/TrTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drrefe\Tr\Test;
+namespace Drrefe\Tr\Tests;
 
 use Drrefe\Tr\Tr;
 use PHPUnit\Framework\TestCase;
@@ -11,7 +11,7 @@ class TrTest extends TestCase
 	public function testSuffix()
 	{
 		$rows = json_decode(file_get_contents(__DIR__ . "/test-rows.json"), true, JSON_UNESCAPED_UNICODE);
-		
+
 		foreach ($rows as $row) {
 			$this->assertSame($row["expected_e"],   Tr::suffix("e",   $row["text"], ["locale" => $row["locale"], "apostrophe" => $row["apostrophe"], "uppercase" => $row["uppercase"]]));
 			$this->assertSame($row["expected_i"],   Tr::suffix("i",   $row["text"], ["locale" => $row["locale"], "apostrophe" => $row["apostrophe"], "uppercase" => $row["uppercase"]]));
